6b6e5a682ed6b224e4cd096b471f6ef02467e7de
Erick Clinton <erickleite2009321@gmail.com>
2022-09-20 20:01:11 -0300

    Adding functionality

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import { Background } from './src/components/Background';
 import { Heading } from './src/components/Heading';
+import { Login } from './src/components/Login';
 
 export default function App() {
   return (
-    <Background>
-      <Heading/>
-    </Background>
+
+      
+      <Background>
+        <StatusBar translucent backgroundColor='transparent' barStyle="dark-content"/>
+        <Login/>
+      </Background>
+
   );
 }
