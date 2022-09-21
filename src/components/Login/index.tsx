03fc0d73a34f6512f2981e7d69168f8d6088e89b
Erick Clinton <erickleite2009321@gmail.com>
2022-09-21 12:35:43 -0300

    Adding ReactState

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -3,16 +3,18 @@ import { View, Text, Image, TextInput, TouchableOpacity,Button} from 'react-nati
 import {Logo} from '../../../assets/drawable-xxxhdpi-icon.png'
 import { Background } from '../Background';
 import { styles } from './styles';
-
+import React from 'react';
 export function Login() {
+  const [text, onChangeText] = React.useState("");
+  const [senha, onChangeSenha] = React.useState("");
   return (
 
     <View style={styles.container}>
-        <Image source={require('../../../assets/drawable-xxxhdpi-icon.png')} style={styles.Logo}></Image>
+        <Image source={require('../../../assets/drawable-xxxhdpi-icon.png')} style={styles.Logo} ></Image>
         <Text>Welcome to app!</Text>
-        <TextInput autoCapitalize='sentences' style={styles.Entradas} placeholder="Login"></TextInput>
+        <TextInput autoCapitalize='sentences' style={styles.Entradas} placeholder="Login"  onChangeText={onChangeText} value={text}></TextInput>
 
-        <TextInput autoCapitalize='sentences'  style={styles.Entradas} placeholder="Senha"secureTextEntry></TextInput>
+        <TextInput autoCapitalize='sentences'  style={styles.Entradas} placeholder="Senha"secureTextEntry onChangeText={onChangeSenha} value={senha}></TextInput>
 
         <TouchableOpacity style={styles.Botao} activeOpacity={0.8}>
             <Text style={styles.Texto}>Entrar</Text>
