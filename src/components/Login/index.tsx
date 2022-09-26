1580f155ab9ac3ae1692c677106ed4f04991b648
Erick Clinton <erickleite2009321@gmail.com>
2022-09-26 19:58:07 -0300

    Completed Login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -5,19 +5,44 @@ import { Background } from '../Background';
 import { styles } from './styles';
 import React from 'react';
 export function Login() {
-  const [text, onChangeText] = React.useState("");
-  const [senha, onChangeSenha] = React.useState("");
+  const [text, setText] = React.useState("");
+  const [senha, setSenha] = React.useState("");
+
+
+  function teste(){
+      console.log(text)
+      console.log(senha)
+  }
   return (
 
     <View style={styles.container}>
         <Image source={require('../../../assets/drawable-xxxhdpi-icon.png')} style={styles.Logo} ></Image>
         <Text>Welcome to app!</Text>
-        <TextInput autoCapitalize='sentences' style={styles.Entradas} placeholder="Login"  onChangeText={onChangeText} value={text}></TextInput>
+        <TextInput 
+          autoCapitalize='sentences' 
+          style={styles.Entradas} 
+          placeholder="Login"  
+          onChangeText={setText}
+          value={text}
+          />
+          
+          
+      
 
-        <TextInput autoCapitalize='sentences'  style={styles.Entradas} placeholder="Senha"secureTextEntry onChangeText={onChangeSenha} value={senha}></TextInput>
-
-        <TouchableOpacity style={styles.Botao} activeOpacity={0.8}>
-            <Text style={styles.Texto}>Entrar</Text>
+        <TextInput autoCapitalize='sentences'  
+          style={styles.Entradas} 
+          placeholder="Senha"
+          secureTextEntry 
+          onChangeText={setSenha}
+          value={senha}
+          
+        />
+          
+        
+        
+        <TouchableOpacity style={styles.Botao} activeOpacity={0.8} onPress={teste} >
+          
+          <Text style={styles.Texto}>Entrar</Text>
         </TouchableOpacity>
 
         
